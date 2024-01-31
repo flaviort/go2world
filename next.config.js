9f4ac85b5d13b74ee64c9c04d7a5913aee76f97c
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2024-01-30 22:24:06 -0300

    recoil message fix

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-	//reactStrictMode: true,
-	output: 'export',
-  	env: {
-		// @see https://github.com/facebookexperimental/Recoil/issues/2135#issuecomment-1362197710
-		RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: false
-  	},
-
-	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-	// trailingSlash: true,
-	
-	// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-	// skipTrailingSlashRedirect: true,
-	
-	// Optional: Change the output directory `out` -> `dist`
-	// distDir: 'dist',
-}
-
-module.exports = nextConfig
-
 const path = require('path')
 
 module.exports = {
 	//assetPrefix: './',
+	//basePath: process.env.PUBLIC_URL,
+	env: {
+		RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: false
+  	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'assets/scss')],
 		prependData: `@import 'atoms/variables.scss';`
@@ -31,7 +13,6 @@ module.exports = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	//basePath: process.env.PUBLIC_URL,
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/i,

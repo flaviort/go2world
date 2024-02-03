176a9bdfe43f043617d050d58a923d8f42cf25dc
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2024-02-03 15:09:19 -0300

    mobile form fixes

--- components/utils/modal/useModal.jsx
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react'
-
-export default function useModal() {
-    const ref = useRef(null)
-    const onOpen = () => ref.current.showModal()
-    const onClose = () => ref.current.close()
-
-    return { ref, onOpen, onClose }
-}
